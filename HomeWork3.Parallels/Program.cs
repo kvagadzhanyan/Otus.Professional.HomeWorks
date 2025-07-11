56979c1d8e368a03f0b5865a1cf329df51762f3c
Karen.Agadzhanyan <kvagadzhanyan@yandex.ru>
2025-07-11 15:55:26 +0300

    HomeWork3.Parallels fixed

--- HomeWork3.Parallels/Program.cs
+++ HomeWork3.Parallels/Program.cs
@@ -6,31 +6,32 @@ namespace HomeWork3.Parallels
     {
         static void Main(string[] args)
         {            
-            Work1();
-            Work2();
+            Work();
+            Work(false);
             Console.ReadKey();
         }
 
-        static void Work1()
+        static void Work(bool isDefaultFolder = true)
         {
-            Console.WriteLine("Задание 1. Обработка трех файлов.");
-            var stopwatch = Stopwatch.StartNew();            
-            var defaultFilesDirectory = $"{Directory.GetCurrentDirectory()}\\testsDirectory";
-            var result = FileHelpers.CountSimbolsInFolder(defaultFilesDirectory, ' ').Result;
-            stopwatch.Stop();
-            Console.WriteLine($"Результат обработки первого задания = {result}. Обработка заняла {stopwatch.ElapsedMilliseconds} мс.");
-        }
-
-        static void Work2()
-        {
-            Console.Write("Введите путь каталога для подсчета символов в файлах: ");
-            var directory = Console.ReadLine();
-            Console.Write("Введите символы для поиска: ");
-            var simbols = Console.ReadLine();
+            var directory = $"{Directory.GetCurrentDirectory()}\\testsDirectory";
+            var simbols = " ";
+            if (isDefaultFolder)
+            {
+                Console.WriteLine("Задание 1. Обработка трех файлов.");
+            }
+            else
+            {
+                Console.Write("Введите путь каталога для подсчета символов в файлах: ");
+                directory = Console.ReadLine();
+                Console.Write("Введите символ для поиска: ");
+                simbols = Console.ReadLine();
+            }
+            
             var stopwatch = Stopwatch.StartNew();
             var result = FileHelpers.CountSimbolsInFolder(directory, string.IsNullOrEmpty(simbols) ? ' ' : simbols[0]).Result;
             stopwatch.Stop();
-            Console.WriteLine($"Результат обработки второго задания = {result}. Обработка заняла {stopwatch.ElapsedMilliseconds} мс.");
+
+            Console.WriteLine($"Результат обработки всех файлов = {result}. Обработка заняла {stopwatch.ElapsedMilliseconds} мс.");
         }
     }
 }
